b462fc63ad7078e57dd2061d06489fbb312fe042
Mike <mmcshane10@gmail.com>
2019-09-06 10:54:53 -0700

    add remaining life expectancy specs

--- spec/spec.js
+++ spec/spec.js
@@ -22,14 +22,44 @@ describe('Space Age Calculator', function() {
     expect(testUser.jupiterCalculator()).toEqual('3');
   });
 
-  it('should calculate the users remaining years on a planet if their age is less than the life expectancy', function() {
+  it('should calculate the users remaining years on mercury if their age is less than the life expectancy', function() {
+    let testUser = new SpaceCalculator("Male", 30)
+    expect(testUser.mercuryLifeExpectancy()).toEqual(192);
+  });
+
+  it('should calculate how far a use has outlived their life expectancy on mercury if their age is higher than the life expectancy', function() {
+    let testUser = new SpaceCalculator("Female", 99)
+    expect(testUser.mercuryLifeExpectancy()).toEqual(75);
+  });
+
+  it('should calculate the users remaining years on venus if their age is less than the life expectancy', function() {
     let testUser = new SpaceCalculator("Male", 30)
     expect(testUser.venusLifeExpectancy()).toEqual(75);
   });
 
-  it('should calculate how far a use has outlived their life expectancy if their age is higher than the life expectancy', function() {
+  it('should calculate how far a use has outlived their life expectancy on venus if their age is higher than the life expectancy', function() {
     let testUser = new SpaceCalculator("Female", 99)
     expect(testUser.venusLifeExpectancy()).toEqual(29);
+  });
+
+  it('should calculate the users remaining years on mars if their age is less than the life expectancy', function() {
+    let testUser = new SpaceCalculator("Male", 30)
+    expect(testUser.marsLifeExpectancy()).toEqual(24);
+  });
+
+  it('should calculate how far a use has outlived their life expectancy on mars if their age is higher than the life expectancy', function() {
+    let testUser = new SpaceCalculator("Female", 99)
+    expect(testUser.marsLifeExpectancy()).toEqual(10);
+  });
+
+  it('should calculate the users remaining years on jupiter if their age is less than the life expectancy', function() {
+    let testUser = new SpaceCalculator("Male", 30)
+    expect(testUser.jupiterLifeExpectancy()).toEqual(4);
+  });
+
+  it('should calculate how far a use has outlived their life expectancy on jupiter if their age is higher than the life expectancy', function() {
+    let testUser = new SpaceCalculator("Female", 99)
+    expect(testUser.jupiterLifeExpectancy()).toEqual(1);
   });
 
 });
