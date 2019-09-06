ee366ddb6d2a8e85e2619b8ec203ec028ddf3717
Mike <mmcshane10@gmail.com>
2019-09-06 13:43:15 -0700

    form input now creates new object when submitted

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+import { SpaceCalculator } from './backend';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  // $("").submit(function(event) {
+  $("form#galaxy").submit(function(event) {
     event.preventDefault();
+    let userSex = $("#user-sex").val();
+    let userAge = $("#user-age").val();
+
+    let newUser = new SpaceCalculator(userSex, userAge);
+    console.log(newUser);
+  });
 });
