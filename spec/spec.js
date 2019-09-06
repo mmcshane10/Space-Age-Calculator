65c7ad1b9ced0a2bb48f308c30820cd0bee40ddc
Mike <mmcshane10@gmail.com>
2019-09-06 09:01:17 -0700

    add all classes for returning different ages.

--- spec/spec.js
+++ spec/spec.js
@@ -9,17 +9,17 @@ describe('Space Age Calculator', function() {
 
   it('should calculate the users age on Venus', function() {
     let testUser = new SpaceCalculator("Male", 30)
-    expect(testUser.mercuryCalculator()).toEqual(48);
+    expect(testUser.venusCalculator()).toEqual(48);
   });
 
   it('should calculate the users age on Mars', function() {
     let testUser = new SpaceCalculator("Male", 30)
-    expect(testUser.mercuryCalculator()).toEqual(16);
+    expect(testUser.marsCalculator()).toEqual(16);
   });
 
   it('should calculate the users age on Jupiter', function() {
     let testUser = new SpaceCalculator("Male", 30)
-    expect(testUser.mercuryCalculator()).toEqual(3);
+    expect(testUser.jupiterCalculator()).toEqual(3);
   });
 
 
